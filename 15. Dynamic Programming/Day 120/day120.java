b81b1a9afee5f3b88e58772993824652faa33935
Unknown Guy <91245369+unknownguy49@users.noreply.github.com>
2025-03-14 21:32:03 +0530

    added day 120

---
+++ 15. Dynamic Programming/Day 120/day120.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int count(int coins[], int sum) {
+        // code here.
+        int dp[]=new int[sum+1]; 
+        dp[0]=1; 
+        for(int coin:coins)
+        { 
+            for(int j=coin;j<=sum;j++)
+            {
+                dp[j]+=dp[j-coin];
+            }
+        } 
+        return dp[sum]; 
+    }
+}
